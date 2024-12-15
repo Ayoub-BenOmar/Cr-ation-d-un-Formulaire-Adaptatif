0d139b0cd961c1bc2d7bb4effcc476aa58db6ca2
ayuub <ayoubbenomar1230@gmail.com>
2024-12-15 22:58:16 +0100

    create interface with html and css native

---
+++ script.js
@@ -0,0 +1,15 @@
+let form = document.getElementById("form");
+
+let userList = [];
+
+form.addEventListener("submit", function(){
+
+    let name = document.getElementById("name").value.trim();
+    let lastName = document.getElementById("last_name").value.trim();
+    let sex = document.getElementById("sex").value  ;
+    let birthDate = document.getElementById("birth_date").value;
+    let email = document.getElementById("email").value.trim();
+    let nationality = document.getElementById("nationality").value.trim();
+
+    
+})
